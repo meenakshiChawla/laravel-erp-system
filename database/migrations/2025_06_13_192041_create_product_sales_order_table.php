59363700b4722396d068dfa01ed93746ca4a3cb2
meenakshiChawla <98146612+meenakshiChawla@users.noreply.github.com>
2025-06-14 09:32:07 +0530

    Update 2025_06_13_192041_create_product_sales_order_table.php

--- database/migrations/2025_06_13_192041_create_product_sales_order_table.php
+++ database/migrations/2025_06_13_192041_create_product_sales_order_table.php
@@ -17,7 +17,8 @@ return new class extends Migration
             $table->unsignedBigInteger('product_id');
             $table->integer('quantity');
             $table->decimal('price', 10, 2);
-            
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
             $table->foreign('sales_order_id')->references('id')->on('sales_orders')->onDelete('cascade');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
