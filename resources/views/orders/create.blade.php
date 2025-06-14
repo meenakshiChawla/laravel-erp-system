38ec45832db964c02b246489045ce29a16d8f3ed
meenakshiChawla <98146612+meenakshiChawla@users.noreply.github.com>
2025-06-14 09:35:24 +0530

    Update create.blade.php

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -9,7 +9,7 @@
             <div class="mb-3">
                 <label>{{ $product->name }} (Stock: {{ $product->quantity }})</label>
                 <input type="number" name="products[{{ $loop->index }}][id]" value="{{ $product->id }}" hidden>
-                <input type="number" required name="products[{{ $loop->index }}][quantity]" class="form-control" placeholder="Enter quantity" min="0">
+                <input type="number" name="products[{{ $loop->index }}][quantity]" class="form-control" placeholder="Enter quantity" min="0">
             </div>
         @endforeach
         <button type="submit" class="btn btn-primary">Create Order</button>
